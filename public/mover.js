dff0ccbdfb536accecf15f7135ea2a4280b1a5b7
Missy Bridgwater <missy789@g.ucla.edu>
2024-05-08 01:22:19 -0400

    Near complete part 3, tho maybe not best format

--- public/mover.js
+++ public/mover.js
@@ -9,6 +9,7 @@ export default class Mover {
     // const startMoveButton = this.m_card.htmlClone.childNodes[7].childNodes[3]; // ??? change this to query selector?
     console.log("LOOK HERE!!!!!!!!!!!");
     console.log(this);
+    this.canMove = true; // track if we are moving
     // const startMoveButton = 
     // startMoveButton.addEventListener("click", (event) => { this.startMoving(); });
 
@@ -34,11 +35,83 @@ export default class Mover {
     function handleMove(event) {
       console.log("did click work?");
       console.log(this);
+      console.log(event.currentTarget)
+      console.log(card);
+      // event.currentTarget.insertAdjacentElement("afterend", card.htmlClone); // this refers to the button, not the class
+      event.currentTarget.after(card.htmlClone);
+      const moveHereButtons = document.getElementsByClassName("moveHere");
+      console.log("moveHereButtons !!!!!!!!!!!!!!!!");
+      console.log(moveHereButtons);
+      const len = moveHereButtons.length;
+      for(let i = 0; i < len; i++) { 
+        moveHereButtons[0].remove();
+      }
+      this.canMove = true;
+    }
+    if (this.canMove) {
+      for(let col = 0; col < colTitles.length; col++) {
+          // make it under parent column and sibling to coltitles
+          // parent.insertBefore(newElem, sibling);
+          let clone = moveButton.cloneNode(true);
+          clone.addEventListener("click", handleMove.bind(this));
+          // colTitles[col].insertAdjacentElement("afterend", clone);
+          colTitles[col].after(clone);
+          // ??? what is the benefit to having this as a member fxn like above or not like here
+      }
+      for(let i = 0; i < cards.length - 1; i++) { // -1 bc this includes the template 
+        let clone = moveButton.cloneNode(true);
+        clone.addEventListener("click", handleMove.bind(this));  // ! .bind(this) maybe so that this refers to mover obj??? also it says to bind in constructor but i didnt do that...
+        // cards[i].insertAdjacentElement("afterend", clone);
+        cards[i].after(clone);
+        // ??? what is the benefit to having this as a member fxn like above or not like here
+
+        // cards[i].appendChild(clone);
+      }
+    }
+    this.canMove = false;  // ! reset to true when we end move
+  }
+
+  stopMoving() {
+    //TODO
+  }
+
+  //TODO
+}
+
+
+
+/* BELLOW HERE IS AN IMPLEMENTATION WITH MOST STUFF MOVED TO CONSTRUCTOR, NOT RIGHT THOUGH */
+
+/*
+/* Text to add to the move here button
+const MOVE_HERE_TEXT = "— Move here —";
+
+// !!! make sure only one set of move buttons can pop up at once
+
+export default class Mover {
+  constructor() {
+    //TODO
+    // const startMoveButton = this.m_card.htmlClone.childNodes[7].childNodes[3]; // ??? change this to query selector?
+    console.log("LOOK HERE!!!!!!!!!!!");
+    console.log(this);
+    // const startMoveButton = 
+    // startMoveButton.addEventListener("click", (event) => { this.startMoving(); });
+
+    function handleMove(event) {
+      console.log("did click work?");
+      console.log(this);
       console.log(card);
       this.insertAdjacentElement("afterend", card.htmlClone); // this refers to the button, not the class
 
-      return -1;
     }
+
+    // ! ?? confused what is supposed to go in here/what this class rly represents ()
+    const moveButton = document.createElement("button");
+    moveButton.textContent = MOVE_HERE_TEXT;
+    moveButton.className = "hidden moveHere";
+    const colTitles = document.getElementsByClassName("columnTitle");
+    const cards = document.getElementsByTagName("article"); // this a good way to access this?
+    
 
     for(let col = 0; col < colTitles.length; col++) {
       // make it under parent column and sibling to coltitles
@@ -57,6 +130,54 @@ export default class Mover {
 
       // cards[i].appendChild(clone);
     }
+
+
+  }
+
+  startMoving(card) {
+    console.log("--entering startMoving--");
+    card.htmlClone.className = "moving";  // give the card the CSS class movingz
+    console.log(card.htmlClone);
+    moveHereButtons = document.getElementsByClassName("moveHere");
+    for(let i = 0; i < moveHereButtons.length; i++) {
+      moveHereButtons[i].className = "moveHere";
+    }
+    // const moveButton = document.createElement("button");
+    // moveButton.textContent = MOVE_HERE_TEXT;
+    // moveButton.className = "moveHere";
+    // console.log(moveButton);
+    // const cols = document.getElementsByClassName("column");
+    // const colTitles = document.getElementsByClassName("columnTitle");
+    // const cards = document.getElementsByTagName("article"); // this a good way to access this?
+    // console.log(colTitles);
+    // console.log(cards);
+
+    // nested function to handle click on Move here buttons 
+    // function handleMove(event) {
+    //   console.log("did click work?");
+    //   console.log(this);
+    //   console.log(card);
+    //   this.insertAdjacentElement("afterend", card.htmlClone); // this refers to the button, not the class
+
+    // }
+
+    // for(let col = 0; col < colTitles.length; col++) {
+    //   // make it under parent column and sibling to coltitles
+    //   // parent.insertBefore(newElem, sibling);
+    //   let clone = moveButton.cloneNode(true);
+    //   clone.addEventListener("click", handleMove);
+    //   colTitles[col].insertAdjacentElement("afterend", clone);
+    //    // ??? what is the benefit to having this as a member fxn like above or not like here
+      
+    // }
+    // for(let i = 0; i < cards.length - 1; i++) { // -1 bc this includes the template 
+    //   let clone = moveButton.cloneNode(true);
+    //   clone.addEventListener("click", handleMove);  // .bind(this) maybe so that this refers to mover obj?? also it says to bind in constructor but i didnt do that...
+    //   cards[i].insertAdjacentElement("afterend", clone);
+    //    // ??? what is the benefit to having this as a member fxn like above or not like here
+
+    //   // cards[i].appendChild(clone);
+    // }
   }
 
   quickTest() {
@@ -69,3 +190,5 @@ export default class Mover {
 
   //TODO
 }
+
+*/
